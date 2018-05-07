bec12a245cf5745072ffd8c9afb64c30f9a81b25
DanielCaetano <37419231+DanielCaetano@users.noreply.github.com>
2018-05-07 16:32:25 -0300

    Add files via upload

--- src/principal/Aluno.java
+++ src/principal/Aluno.java
@@ -1,7 +1,41 @@
 
-package principal;
+package Objetos;
 
 
 public class Aluno {
+    private int matricula;
+    private String nome;
+    private String categoria;
+
+    public Aluno(int matricula, String nome, String categoria){
+        setMatricula(matricula);
+        setNome(nome);
+        setCategoria(categoria);
+    }
+    
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    
     
 }
